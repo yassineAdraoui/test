1056b3bbff7f9177f69979a77228afc4a4c21442
yassineAdraoui <62291655+yassineAdraoui@users.noreply.github.com>
2026-01-13 17:36:45 +0100

    Add files via upload

---
+++ components/icons/TelegramIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const TelegramIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg 
+    viewBox="0 0 24 24" 
+    fill="currentColor"
+    {...props}
+  >
+    <path d="M9.78 18.65l.28-4.23 7.68-6.92c.34-.31-.07-.46-.52-.19L7.74 13.3 3.64 12c-.88-.25-.89-.86.2-1.3l15.97-6.16c.73-.33 1.43.18 1.15 1.3l-2.72 12.57c-.24 1.15-1.01 1.43-1.8 1.01l-4.88-3.58-2.32 2.23c-.25.24-.46.45-.88.45z" />
+  </svg>
+);
